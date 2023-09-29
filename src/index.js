9545cfdb12135abe5050c29fa984bb83c2a2e911
Rowland <rowland.wanderi@student.moringaschool.com>
2023-09-29 06:14:01 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    myTodos(input.value);
+    input.value = "";
+  });
 });
+
+function myTodos(todo) {
+  const myList = document.getElementById("tasks");
+  const btn = document.createElement("button");
+  const listItem = document.createElement("li");
+  btn.textContent = " X ";
+  listItem.textContent = ` ${todo} `;
+  listItem.appendChild(btn);
+  myList.appendChild(listItem);
+
+ btn.addEventListener("click", deleteTask);
+}
+
+// delet element function
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
+
+
+
+
+ 
